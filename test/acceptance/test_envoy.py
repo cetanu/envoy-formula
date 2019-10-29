66458892676bd889081f456c511509201c66a205
Vasili Syrakis <cetanu@gmail.com>
2019-10-29 23:59:43 +1100

    fix version check

--- test/acceptance/test_envoy.py
+++ test/acceptance/test_envoy.py
@@ -7,6 +7,9 @@ def test_envoy_binary(host):
     binary = host.file('/usr/bin/envoy')
     assert binary.exists
     assert binary.mode == 0o755
+
+
+def test_envoy_binary_prints_version(host):
     cmd = host.run('/usr/bin/envoy --version')
     assert cmd.succeeded
-    assert cmd.stdout == 'bleh'
+    assert '1.11.2' in cmd.stdout
