d74711fe28cab1bffb040ee31f8574f3e0f09c8f
Vasili Syrakis <cetanu@gmail.com>
2019-10-29 23:35:44 +1100

    Add test for envoy executable

--- test/acceptance/test_envoy.py
+++ test/acceptance/test_envoy.py
@@ -1,3 +1,12 @@
 def test_envoy_service_file(host):
     file = host.file('/etc/systemd/system/envoy.service')
     assert file.exists
+
+
+def test_envoy_binary(host):
+    binary = host.file('/usr/bin/envoy')
+    assert binary.exists
+    assert binary.mode == 0o755
+    cmd = host.run('/usr/bin/envoy --version')
+    assert cmd.succeeded
+    assert cmd.stdout == 'bleh'
